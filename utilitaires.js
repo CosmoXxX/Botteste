b85e1da758fa118be7fa175e21b646d1a34828a5
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-06-18 21:10:29 +0400

    Update utilitaires.js

--- utilitaires.js
+++ utilitaires.js
@@ -22,3 +22,22 @@ if(command === "ping") {
             const m = await newFunction(message)("Ping?");
             m.edit(`Pong! Latence : **${m.createdTimestamp - message.createdTimestamp}ms**. API latence : **${Math.round(client.ping)}ms**`);
           }
+
+ if(command === "annonce") {
+            const sayMessage2 = args.join(" ");
+           
+            let servIcon = message.guild.iconURL;
+
+            message.delete().catch(O_o=>{});
+            const embed = new RichEmbed()
+            .setTitle(':pushpin: Annonce :')
+            .setColor("00CCFF")
+            .setTimestamp()
+            .setThumbnail(servIcon)
+            .setFooter(`Cordialement, ${message.author.username}`)
+            .setDescription(sayMessage2)
+            
+            message.channel.send(embed)
+    
+            
+            }

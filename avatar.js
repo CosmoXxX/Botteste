90f5d7425cee53aed612f45eb7c75e0c80a0f7ef
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-06-18 20:01:15 +0400

    Create avatar.js

---
+++ avatar.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const settings = require('./index.js)
+
+ if (command === 'avatar') {
+                                let mentionedUser = message.mentions.users.first() || message.author;
+                        
+                                let embed = new Discord.RichEmbed()
+                          
+                                .setImage(mentionedUser.displayAvatarURL)
+                                .setColor("00ff00")
+                                .setTitle("Avatar :")
+                                .setDescription("[Lien URL de l'avatar]("+mentionedUser.displayAvatarURL+")")
+                             
+                                message.channel.send(embed)  
+                          }

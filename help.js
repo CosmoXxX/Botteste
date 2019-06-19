c3ede7c6d84f07f18bffa0705bae595246e7ffef
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-06-19 12:10:14 +0400

    Update help.js

--- help.js
+++ help.js
@@ -3,8 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) =>{
 
     let helpembed = new Discord.RichEmbed()
-   .addField('🛠 **Utilitaire **:', '``'avatar [user]`` , ``'bvn [user]`` ,  ``'bot-info`` , ``'serveur-info`` , ``'rôle-info [rôle]`` , ``'google [recherche]`` , ``'youtube [recherche]``')message.channel.send(helpembed);
+   .addField('🛠 **Utilitaire **:', '``'avatar [user]`` , ``'bvn [user]`` ,  ``'bot-info`` , ``'serveur-info`` , ``'rôle-info [rôle]`` , ``'google [recherche]`` , ``'youtube [recherche]``')
    .addField('🎉  **FUN** :', '``'8ball [questions]``')
    .addField('🚓 **Modérations** :', '``'ban [user] [raison]`` , ``'kick [user] [raison]`` , ``'warn [user] [raison]`` , ``'mute [user] [raison]`` , ``'purge [2-100]`` , ``'unmute [user]`` , ``'addrole [user] [role]`` , ``'removerole [user] [role]``')
    .addField('📌  **Autres** :', '``'ping``')
+   message.channel.send(helpembed);
 }

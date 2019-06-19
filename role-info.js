4feec8d4f7daf2f5d0c102d25004d6bca3b6e098
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-06-19 15:11:09 +0400

    Update role-info.js

--- role-info.js
+++ role-info.js
@@ -1,16 +1,22 @@
 
+const Discord = require('discord.js');
 
- client.on('message', async message => {
+
+
+
+const settings = {
+    prefix: ''',
+    token: 'NTkwNTYwNDQwNjU2ODU1MDUz.XQkAag.Jqcj6743NWDGFrdhkFpLOoCsyYs'
+  } 
+    
+    client.on('message', async message => {
     var command = message.content.toLowerCase().slice(settings.prefix.length).split(' ')[0];
     var args = message.content.split(' ').slice(1);
     if(message.author.bot) return;
     if (!message.content.startsWith(settings.prefix) || message.author.bot) return;
 
-if(command === "coucou") {
-message.channel.send("salut")
-}
 
-});
+
 if (command === 'role-info') {
  let inline = true
 
@@ -40,3 +46,5 @@ if (command === 'role-info') {
 
 }
  
+
+)};
